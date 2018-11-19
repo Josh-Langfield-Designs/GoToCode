383aef4c4568f28357b314b7ecf1063f06713524
Josh-Langfield-Designs <43525627+Josh-Langfield-Designs@users.noreply.github.com>
2018-11-19 11:29:41 -0800

    added root files

---
+++ index.php
@@ -0,0 +1,75 @@
+<?php require("includes/noCacheHeader.php"); ?>
+<title>----FIRMNAME----</title>
+<meta name="keywords" content="----FIRMNAME----">
+<meta name="Description" content="----FIRMNAME----">
+<meta name="copyright" content="Integrity Marketing Solutions">
+<meta name="doc-rights" content="Copywritten Work">
+<link rel="canonical" href="https://www.----URL----.com/">
+<?php require("includes/headerScripts.php"); ?>
+</head>
+<body>
+	<?php require("includes/videoBG.php"); ?>
+	<?php require("includes/header1.php"); ?>
+	<div data-sr="enter over 1s no reset" data-sticky-container>
+		<div class="sticky" data-sticky data-sticky-on="medium" data-options="marginTop:0" data-top-anchor="navTop:top" style="width:100%; z-index:99;">
+			<?php require('includes/horizontalNav.php'); ?>
+		</div>
+	</div>
+	<?php require("includes/header2.php"); ?>
+	<?php require("includes/practiceAreasCTA1.php"); ?>
+	<?php require("includes/practiceAreasCTA2.php"); ?>
+	<?php require("includes/practiceAreasCTA3.php"); ?>
+
+
+	<!-- begin bodySection -->
+
+	<section class="bodySection">
+		<div data-sr="enter over 1s no reset" class="grid-container">
+			<div class="grid-x grid-margin-x grid-margin-y align-center">
+				<div class="small-12 large-9 cell">
+
+					<!-- begin anchor for screen readers -->
+
+					<div id="content"></div>
+
+					<!-- end anchor for screen readers -->
+
+					<!-- begin Main Content -->
+
+					<h1>----FIRMNAME----</h1>
+					<hr>
+					<h2>----STATE---- Estate Planning Law Firm &mdash; Serving ----City----  &amp; the Surrounding Areas</h2>
+					<p>We are a full-service estate planning law office working to provide Peace of Mind to our clients and their families. We are committed to personal service to each client, using only those legal tools and techniques that suit the individual client&rsquo;s needs, goals and personal situation.</p>
+					<h3>Services  provided include legal consultation and the preparation of:</h3>
+					<p>Revocable Living Trusts &#8226; Wills &#8226; Powers of Attorney &#8226; Living Wills &#8226; Healthcare Power of Attorney &#8226; Life Insurance Trusts &#8226; Family Limited Partnerships &#8226; Limited Liability Companies &#8226; Corporations &#8226; Charitable Trusts &#8226; Medicaid Planning &#8226; and Other Estate Planning and Tax Planning Strategies</p>
+					<p><a href="Estate-Planning-Practice-Areas.php">Learn more about our services.</a> </p>
+
+					<blockquote>&ldquo;This is a blockquote. A blockquote is this. You're looking at what is called a blockquote. Actually that may not be what these are called.&rdquo;
+						<cite>Jane Dough</cite>
+					</blockquote>
+
+					<?php require("includes/copyright-Law-Firm.php"); ?>
+
+					<!-- End Main Content -->
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- end bodySection -->
+
+	<?php require("includes/callToActionSection.php"); ?>
+	<?php require("includes/callToActionSection2.php"); ?>
+	<div class="parallax-window" data-parallax="scroll" data-bleed="50" data-position="center top" data-image-src="images/pageLayout/bg-main.jpg">
+		<?php require("includes/testimonialSection.php"); ?>
+	</div>
+	<?php require("includes/socialSection.php"); ?>
+	<?php require("includes/memberLogoSection.php"); ?>
+	<?php require("includes/newsletterSection.php"); ?>
+	<?php require("includes/LifeStages.php"); ?>
+	<?php require("includes/contactSection.php"); ?>
+	<?php require("includes/footer.php"); ?>
+	<?php require("includes/footerScripts.php"); ?>
+</body>
+
+</html>
